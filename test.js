94c9ff2fd271874db5b4f1d20517a7b7e6725306
Boyang <wangboyang1991@gmail.com>
2016-07-03 21:35:37 +0800

    test for json

--- test.js
+++ test.js
@@ -11,24 +11,29 @@ let db;
 
 describe('umeranApp', () => {
     before((done) => {
-        if (app.server.listening)
-            connectAndSetMongo().then(done).catch((reason) => done(reason));
-        else
-            app.server.on('listening', () => connectAndSetMongo().then(done).catch((reason) => done(reason)));
+        function setup() {
+            connectAndSetMongo()
+                .then(clearCollection)
+                .then(done)
+                .catch((reason) => done(reason));
+        }
+        app.server.listening ? setup() : app.server.on('listening', setup);
     });
-    describe('when receive request for analytics.png', () => {
-        let url = hostUrl + '/analytics.png', res, previousCount;
+    describe('when receive request for analytics.png then analytics.json', () => {
+        let resPng, resJson, previousCount;
         let timestamp1MinuteBefore = new Date(new Date().setMinutes(new Date().getMinutes()-1));
 
         before((done) => {
             db.collection('analyticsPngRaw').count({timestamp: {$gte: timestamp1MinuteBefore}})
                 .then((count) => previousCount = count)
-                .then(() => rp({uri: url, resolveWithFullResponse: true}))
-                .then((response) => void (res = response))
+                .then(() => rp({uri: hostUrl + '/analytics.png', resolveWithFullResponse: true}))
+                .then((response) => void (resPng = response))
+                .then(() => rp({uri: hostUrl + '/analytics.json', resolveWithFullResponse: true}))
+                .then((response) => void (resJson = response))
                 .then(done);
         });
         it('returns the image', (done) => {
-            expect(res.statusCode).to.equal(200);
+            expect(resPng.statusCode).to.equal(200);
             new Promise((resolve, reject) => {
                 fs.readFile(__dirname + '/white-1x1.png', 'utf8', (err, data) => {
                     if (err)
@@ -37,7 +42,7 @@ describe('umeranApp', () => {
                         resolve(data)
                 });
             })
-                .then((data) => { expect(res.body).to.equal(data); })
+                .then((data) => { expect(resPng.body).to.equal(data); })
                 .then(done)
                 .catch((reason) => done(reason));
         });
@@ -46,10 +51,19 @@ describe('umeranApp', () => {
                 .then((count) => { expect(count).to.equal(previousCount + 1); })
                 .then(done);
         });
+        it('returns json with correct total and #record', () => {
+            expect(resJson.statusCode).to.equal(200);
+            let result = JSON.parse(resJson.body);
+            expect(result.total).to.equal(1);
+            expect(result.records.length).to.equal(1);
+        });
     });
 });
 
 function connectAndSetMongo() {
     return mongodb.MongoClient.connect(dbUrl)
         .then((mongodb) => void (db = mongodb));
+}
+function clearCollection() {
+    return void db.collection('analyticsPngRaw').remove({persistent: {$exists: false}});
 }
