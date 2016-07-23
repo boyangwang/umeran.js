b096a33d8e5e3f905359b7a6679e9071c5488d19
Boyang <wangboyang1991@gmail.com>
2016-07-24 06:23:13 +0800

    added forever script for scraper

--- utilScripts/foreverstart.sh
+++ utilScripts/foreverstart.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
-forever start -a -l /root/code/logs/forever_umeran.log \
-              -o /root/code/logs/forever_umeran.log \
-              -e /root/code/logs/forever_umeran.log \
-              --uid umeran \
-              --pidFile /root/code/logs/umeran.pid \
-              /root/code/umeran.js/app.js
+# forever start -a -l /root/code/logs/forever_umeran.log \
+#               -o /root/code/logs/forever_umeran.log \
+#               -e /root/code/logs/forever_umeran.log \
+#               --uid umeran \
+#               --pidFile /root/code/logs/umeran.pid \
+#               /root/code/umeran.js/app.js
+#! /bin/bash
+forever start -a -l /root/code/logs/forever_umeran_scraper.log \
+              -o /root/code/logs/forever_umeran_scraper.log \
+              -e /root/code/logs/forever_umeran_scraper.log \
+              --uid umeran_scraper \
+              --pidFile /root/code/logs/umeran_scraper.pid \
+              /root/code/umeran.js/scraper/index.js scrapConfig.json
 forever list
 echo "Done"
